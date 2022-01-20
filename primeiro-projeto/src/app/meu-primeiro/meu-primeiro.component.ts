24df255362675e5f50438aaeb0d16632c3325c8e
Ivís Confessor <ivisconfessor@gmail.com>
2022-01-20 15:49:41 -0300

    Primeiro Projeto Finalizado - Hello World

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component com Angular!</p>
+        <p>---------</p>
+    `
+})
+
+export class MeuPrimeiroComponent {
+
+}
