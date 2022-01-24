f68b39233fcc37276aff877beb29f32617976399
Ivís Confessor <ivisconfessor@gmail.com>
2022-01-24 16:09:36 -0300

    Hello Typescrípt - Conhecendo o Typescript

---
+++ hello-typescript/main.ts
@@ -0,0 +1,27 @@
+// JS Puro
+
+var minhaVar = 'minha variavel'; 
+
+function minhaFunc(x, y) { 
+    return x + y;
+}
+
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2; 
+});
+
+// ES 6 ou ES 2015
+let num = 2;
+const PI = 3.14;
+
+numeros.map(valor => valor * 2);
+
+class Matematica {
+    soma(x, y){
+        return x + y;
+    }
+}
+
+var n1: any = 'asdsf';
+n1 = 4;
